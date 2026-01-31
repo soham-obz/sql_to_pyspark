3fa266fa1215a2c5ac8a2e16fae4eab6bcf6629e
soham-obz <soham.gokulamrutkar@onebyzero.ai>
2026-01-31 14:08:59 +0530

    Migrate orders SQL to PySpark

--- src/orders.py
+++ src/orders.py
@@ -1,8 +1,3 @@
-Here's the PySpark DataFrame code equivalent to the given SQL query:
-
 from pyspark.sql.functions import col
 
-df_a = spark.table("a")
-df_b = spark.table("b")
-
-result = df_a.join(df_b, df_a.id == df_b.id).select(df_a.id, df_b.name)
+result = a.join(b, a.id == b.id).select(a.id, b.name)
