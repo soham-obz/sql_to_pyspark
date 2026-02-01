2e49e345a15bd631080fa6ada7c9f8fb1b60da73
soham-obz <soham.gokulamrutkar@onebyzero.ai>
2026-02-01 10:53:49 +0530

    SQL to Pyspark Code migration

--- src/payment.py
+++ src/payment.py
@@ -1,13 +1,6 @@
 from pyspark.sql.functions import col
 
-payments = spark.table("payments").alias("p")
-subscriptions = spark.table("subscriptions").alias("s")
-
-result = payments.join(
-    subscriptions,
-    col("p.sub_id") == col("s.id"),
-    "inner"
-).select(
-    col("p.id"),
-    col("s.status")
-)
+result = spark.table("payments").alias("p") \
+    .join(spark.table("subscriptions").alias("s"), 
+          col("p.sub_id") == col("s.id"), "inner") \
+    .select(col("p.id"), col("s.status"))
